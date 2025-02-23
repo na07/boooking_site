15c83db0d9cd54a43a740d891395ec349d4aa0d9
na07 <nkef2698@gmail.com>
2025-02-23 04:09:30 +0300

    validator + admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Location, Booking
 
-# Register your models here.
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', "capacity", 'description')
+    search_fields = ('title', 'cost', "capacity")
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'location', 'start_date', 'created_at', 'end_field')
+    list_filter = ('start_date', 'location')
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('User')
+    search_fields = ('User')
+    # тут хз если честно
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MinLengthValidator
 from django.db import models
 
+User = get_user_model()
+
 # Create your models here.
+class Location(models.Model):
+    title = models.CharField(max_length=100,
+                             unique=True,
+                             validators=[MinLengthValidator(5, message="минимум 5 букв")])
+    cost = models.DecimalField(max_digits=7,
+                               decimal_places=2,
+                               validators=[MinValueValidator(1, message="минимум 1")])
+    capacity = models.PositiveIntegerField()
+    description = models.TextField()
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    location = models.ForeignKey(Location,
+                                 related_name="bookings",
+                                 on_delete=models.CASCADE,
+                                 validators=[MinLengthValidator(3, message="минимум 3")])
+    user = models.ForeignKey(User, related_name="bookings", on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_field = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.start_date)
